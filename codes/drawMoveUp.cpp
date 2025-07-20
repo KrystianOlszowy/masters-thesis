1f349de0eb37e774bd85a65891e9d86908667f66
KrystianOlszowy <krystianolszowy.praca@gmail.com>
2025-07-20 17:57:16 +0200

    ANTRLv4 for revision

--- codes/drawMoveUp.cpp
+++
@@ -1,8 +0,0 @@
-// rysowanie ikony przycisku przejścia w górę i jego opisu
-void bt2ir::Display::drawMoveUp()
-{
-    this->clearDisplay();
-    const std::string description_text{"Move Up"};
-    drawIconWithDescription(moveUp_icon, description_text);
-    this->display();
-}
